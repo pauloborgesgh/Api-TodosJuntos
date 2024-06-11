241953828049ebe8f64a03a5d7e1acff3f25ad24
pauloborgesgh <79487337+pauloborgesgh@users.noreply.github.com>
2024-06-11 16:14:56 -0300

    Update server.js
    modificando api

--- server.js
+++ server.js
@@ -77,7 +77,10 @@ app.put('/denuncias/:id', async (req, res) => {
            
     }
     })
+
 res.status(200).json(req.body);
+
+
 })
 
 app.delete('/denuncias/:id', async (req, res) => {
@@ -91,6 +94,8 @@ app.delete('/denuncias/:id', async (req, res) => {
 
 
 })
+
+
 //Rotas Usuario Get
 
 app.get('/user', async (req, res) => {
@@ -114,16 +119,74 @@ app.get('/user', async (req, res) => {
 
 
 })
+///verificação 
+app.post('/user/login', async (req, res) => {
+    try {
+      let users = [];
+  
+      // Verificar se os parâmetros de consulta foram fornecidos
+      if (req.body.email && req.body.password) {
+        users = await prisma.user.findMany({
+          where: {
+            email: req.body.email,
+            password: req.body.password,
+            
+            
+          },
+          
+          
+        });
+        console.log('usuario encontrado');
+
+      } else {
+        // Se nenhum parâmetro de consulta foi fornecido, retornar todos os usuários
+        users = await prisma.user.findMany();
+      }
+  
+      // Verificar se há algum usuário retornado
+      if (users.length > 0) {
+        console.log('Usuário encontrado:', users[0].email); // Adicionando um console.log aqui
+        res.status(200).json(users);
+      } else {
+        res.status(404).json({ message: 'Usuário não encontrado' });
+      }
+    } catch (error) {
+      console.error('Erro:', error);
+      res.status(500).json({ message: 'Erro interno do servidor' });
+    }
+  });
+  
+
+
 
 //Rotas Usuario Post
 
+// app.post('/user', async (req, res) => {
+
+//     await prisma.user.create({
+//         data: {
+//             email:req.body.email,
+//             name:req.body.email,
+//             password: req.body.password,
+//             re_password: req.body.re_password,
+           
+           
+//         }
+//     })
+//     res.status(201).json(req.body);
+
+
+// })
+
+//registro de usuario
 app.post('/user', async (req, res) => {
 
     await prisma.user.create({
         data: {
-            email:req.body.email,
             name:req.body.email,
+            email:req.body.email,
             password: req.body.password,
+            re_password: req.body.re_password,
            
         }
     })
@@ -133,8 +196,31 @@ app.post('/user', async (req, res) => {
 })
 
 
+app.get('/user/revr', async (req, res) => {
+    let register = []
+
+    if (req.query) {
+        register = await prisma.register.findMany({
+            where: {
+                email: req.query.email,
+                nome: req.query.nome,
+                password : req.query.password,
+     
+            }
+        })
+    } else {
+        const register = await prisma.register.findMany()
+    }
+
+
+    res.status(200).json(register);
+
+
+})
 
 app.listen(3000, () => {
     console.log('servidor dodando port 3000');
 })
+
+
 
